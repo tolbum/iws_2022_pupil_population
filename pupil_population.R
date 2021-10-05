d1331078fa529fbb9e513b46ff657b673c8f9f0f
Tolulope Abolade <taabolade@gmail.com>
2021-10-05 04:15:37 +0100

    Added a new column to dataset

--- pupil_population.R
+++ pupil_population.R
@@ -11,10 +11,9 @@ pacman::p_load("rio",
 # load population data
 # import data sheets and assign results as school_list_
 
-school_list_raw <- openxlsx::getSheetNames("school_list.xlsx")
-
 library(readxl)
 library(lubridate)
+library(ggplot2)
 
 school_list_creche <- read_excel("school_list.xlsx", sheet = 2, skip = 7)
 school_list_playgrp <- read_excel("school_list.xlsx", sheet = 3, skip = 7)
@@ -22,6 +21,15 @@ school_list_prep <- read_excel("school_list.xlsx", sheet = 4, skip = 7)
 school_list_nursery_1 <- read_excel("school_list.xlsx", sheet = 5, skip = 7)
 school_list_nursery_2 <- read_excel("school_list.xlsx", sheet = 6, skip = 7)
 school_list_primary_1 <- read_excel("school_list.xlsx", sheet = 7, skip = 7)
+
+class_arm_list <- openxlsx::getSheetNames("school_list.xlsx")
+
+class_arm_creche <- class_arm_list[2]
+class_arm_playgrp <- class_arm_list[3]
+class_arm_prep <- class_arm_list[4]
+class_arm_nursery_1 <- class_arm_list[5]
+class_arm_nursery_2 <- class_arm_list[6]
+class_arm_primary_1 <- class_arm_list[7]
 
 ## Cleaning Pipe
 
@@ -31,6 +39,8 @@ creche_list <- school_list_creche %>%
   janitor::clean_names() %>%
   
   select(!"s_n") %>%
+  
+  mutate(class_arm = class_arm_creche) %>%
   
   mutate(birthday_date = as.Date(birthday_date, origin = "1970-01-01")) %>%
 
@@ -43,7 +53,9 @@ creche_list <- school_list_creche %>%
 playgrp_list <- school_list_playgrp %>%
   janitor::clean_names() %>%
 
-select(!"s_n") %>%
+  select(!"s_n") %>%
+  
+  mutate(class_arm = class_arm_playgrp) %>%
 
   mutate(birthday_date = as.Date(birthday_date, origin = "1970-01-01")) %>%
   
@@ -58,6 +70,8 @@ prep_list <- school_list_prep %>%
   
   select(!"s_n") %>%
   
+  mutate(class_arm = class_arm_prep) %>%
+  
   mutate(birthday_date = as.Date(birthday_date, origin = "1970-01-01")) %>%
   
   filter(!name == "NA") %>%
@@ -70,6 +84,8 @@ nursery_1_list <- school_list_nursery_1 %>%
   janitor::clean_names() %>%
   
   select(!"s_n") %>%
+  
+  mutate(class_arm = class_arm_nursery_1) %>%
   
   mutate(birthday_date = as.Date(birthday_date, origin = "1970-01-01")) %>%
   
@@ -84,6 +100,8 @@ nursery_2_list <- school_list_nursery_2 %>%
   
   select(!"s_n") %>%
   
+  mutate(class_arm = class_arm_nursery_2) %>%
+  
   mutate(birthday_date = as.Date(birthday_date, origin = "1970-01-01")) %>%
   
   filter(!name == "NA") %>%
@@ -95,7 +113,9 @@ nursery_2_list <- school_list_nursery_2 %>%
 primary_1_list <- school_list_primary_1 %>%
   janitor::clean_names() %>%
 
-select(!"s_n") %>%
+  select(!"s_n") %>%
+  
+  mutate(class_arm = class_arm_primary_1) %>%
   
   mutate(birthday_date = as.Date(birthday_date, origin = "1970-01-01")) %>%
   
@@ -113,4 +133,19 @@ school_list_cleaned <- bind_rows(creche_list,
                             nursery_1_list, 
                             nursery_2_list, 
                             primary_1_list)
+
+school_list_cleaned %>%
+  
+  theme_bw() +
+  
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(), 
+        axis.line = element_line()) +
+  
+  ggplot(aes(x=class_arm)) +
+  
+  geom_bar(aes(fill = gender)) +
+  
+  facet_grid(rows = vars(status))
 
